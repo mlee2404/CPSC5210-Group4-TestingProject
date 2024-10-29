965b8b9b1c38b81fc12563e2abc06b96257eb960
mlee2328 <146597565+mlee2404@users.noreply.github.com>
2024-10-28 20:39:06 -0700

    Added more BoardTests

--- src/test/BoardTests.java
+++ src/test/BoardTests.java
@@ -6,6 +6,12 @@ import src.othello.Board;
 
 public class BoardTests {
     @Test
+    void inBoundsTest() {
+        Board board = new Board();
+        Assertions.assertTrue(board.inBounds(5, 5));
+    }
+
+    @Test
     void inBoundsLowerBoundTest() {
         Board board = new Board();
         Assertions.assertTrue(board.inBounds(0, 0));
@@ -18,14 +24,56 @@ public class BoardTests {
     }
 
     @Test
-    void inBoundsOutOfLowerBoundTest() {
+    void inBoundsOutOfLowerBoundTest1() {
         Board board = new Board();
         Assertions.assertFalse(board.inBounds(9, -1));
     }
 
     @Test
-    void inBoundsOutOfUpperBoundTest() {
+    void inBoundsOutOfLowerBoundTest2() {
+        Board board = new Board();
+        Assertions.assertFalse(board.inBounds(-1, 9));
+    }
+
+    @Test
+    void inBoundsOutOfLowerBoundTest3() {
+        Board board = new Board();
+        Assertions.assertFalse(board.inBounds(-1, -1));
+    }
+
+    @Test
+    void inBoundsOutOfUpperBoundTest1() {
         Board board = new Board();
         Assertions.assertFalse(board.inBounds(10, 0));
+    }
+
+    @Test
+    void inBoundsOutOfUpperBoundTest2() {
+        Board board = new Board();
+        Assertions.assertFalse(board.inBounds(0, 10));
+    }
+
+    @Test
+    void inBoundsOutOfUpperBoundTest3() {
+        Board board = new Board();
+        Assertions.assertFalse(board.inBounds(10, 10));
+    }
+
+    @Test
+    void isEmptyTest() {
+        Board board = new Board();
+        Assertions.assertTrue(board.isEmpty(0,0));
+    }
+
+    @Test
+    void isEmptyNotEmptyTest() {
+        Board board = new Board();
+        Assertions.assertFalse(board.isEmpty(4,4));
+    }
+
+    @Test
+    void isFilledTest() {
+        Board board = new Board();
+        Assertions.assertFalse(board.isFilled());
     }
 }
